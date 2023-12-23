876e0b53851cbe51a4ee1a8f3ee8c70c2f5bcbeb
denniskioko <kiokodennis6990@gmail.com>
2023-12-23 20:35:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Guadalipe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+
+    names[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return names;
+}
+
+writeCards(names);
+
+
+function countDown(num) {
+    let i = 0
+    while (i <= num) {
+        console.log(num--);
+    }
+}
